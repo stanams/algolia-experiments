a28ceda34e1752620bb3f50751a4611121885eb6
StanWDG <stan.amsellem@gmail.com>
2017-05-28 19:06:13 +0200

    connecting alogliasearch client

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,41 @@ import { SearchBar } from './Components/SearchBar/SearchBar.component'
 import { SearchResultsList } from './Components/SearchResultsList/SearchResultsList.component'
 import { AddMovieButton } from './Components/AddMovieButton/AddMovieButton.component'
 import { AddMovieForm } from './Components/AddMovieForm/AddMovieForm.component'
+import algoliasearch from 'algoliasearch'
 
+const client = algoliasearch("J2ZJK8FEIS", "399b6ba8d0bf23ef41f3314ca025fa0a");
+const index = client.initIndex('Movie');
 
 class App extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      isFormOpen: false
+      isFormOpen: false,
+      query: '',
+      results: {}
     }
     this.openForm = this.openForm.bind(this)
     this.closeForm = this.closeForm.bind(this)
+    this.handleInput = this.handleInput.bind(this)
+    this.search = this.search.bind(this)
+  }
+
+
+  handleInput(e) {
+    const query = e.target.value
+    this.setState({ query: query })
+    this.search(query)
+  }
+
+  search(query) {
+    index.search(query, function searchDone(err, content) {
+    if (err) {
+      console.error(err)
+      return
+    }
+    this.setState({ results: content })
+    }.bind(this))
   }
 
   openForm() {
@@ -27,16 +51,15 @@ class App extends Component {
   }
 
   render() {
-    const { isFormOpen } = this.state
-    console.log(isFormOpen)
+    const { isFormOpen, query, results } = this.state
     return (
       <div className="App">
         <Header/>
         <div className="search-container">
-          <SearchBar/>
+          <SearchBar query={ query } handleInput={ this.handleInput } />
           <AddMovieButton openForm={ this.openForm }/>
         </div>
-        <SearchResultsList/>
+        <SearchResultsList results={ results }/>
         { isFormOpen &&
           <AddMovieForm closeForm={ this.closeForm }/>
         }
--- client/src/Components/SearchBar/SearchBar.component.js
+++ client/src/Components/SearchBar/SearchBar.component.js
@@ -2,10 +2,16 @@ import React from 'react'
 import { Icon } from 'semantic-ui-react'
 import './SearchBar.component.css'
 
-export const SearchBar = () => {
+export const SearchBar = ({ query, handleInput }) => {
+
   return (
     <div className="search-bar">
-      <Icon name="search" color="grey" className="search-bar-icon"/><input className="search-bar-input" type="text" placeholder="Search by title: Titanic, Matrix..."/>
+      <Icon name="search"color="grey" className="search-bar-icon"/>
+      <input onChange={ handleInput }
+             value={ query }
+             className="search-bar-input"
+             type="text"
+             placeholder="Search by title: Titanic, Matrix..."/>
     </div>
   )   
 }
